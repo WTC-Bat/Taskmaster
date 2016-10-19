3362d66c7ccbb8b929a8265faa11173498b02c2a
Matthew Williams <FatBat@Matthews-iMac.local>
2016-10-19 18:35:45 +0200

    python tm

---
+++ python/program_class.py
@@ -0,0 +1,56 @@
+import array
+
+class Program():
+	""""""
+	command = ""
+	procnum = 1
+	autolaunch = True
+	starttime = 5
+	restart = "unexpected"
+	retries = 3
+	stopsig = ""
+	stoptime = 10
+	exitcodes = list()
+	stdout = ""
+	stderr = ""
+	redir = ""
+	envvars = dict()
+	workingdir = ""
+	umask = ""
+
+	def __init__(self, args):
+		""""""
+		if type(args) is dict:
+			for key, val in args.iteritems():
+				if key == "command":
+					self.command = val
+				elif key == "procnum":
+					self.procnum = val
+				elif key == "autolaunch":
+					self.autolaunch = val
+				elif key == "starttime":
+					self.starttime = val
+				elif key == "restart":
+					self.restart = val
+				elif key == "retries":
+					self.retries = val
+				elif key == "stopsig":
+					self.stopsig = val
+				elif key == "stoptime":
+					self.stoptime = val
+				elif type(val) is list and key == "exitcodes":
+					for i in val:
+						self.exitcodes.append(i);
+				elif key == "stdout":
+					self.stdout = val
+				elif key == "stderr":
+					self.stderr = val
+				elif key == "redir":
+					self.redir = val
+				elif type(val) is dict and key == "envvars":
+					for k, v in val.iteritems():
+						self.envvars[k] = v
+				elif key == "workingdir":
+					self.workingdir = val
+				elif key == "umask":
+					self.umask = val
---
+++ python/taskmaster.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os
+import cmd
+
+
+class Taskmaster(cmd.Cmd):
+	""""""
+	def __init__(self):
+		""""""
+		cmd.Cmd.__init__(self)
+		self.prompt = "Taskmaster> "
+
+	def emptyline(self):
+		""""""
+		pass
+
+	def do_hist(self, args):
+		""""""
+		print self._hist
+
+	"""
+	def preloop(self):
+		""""""
+	"""
+
+	"""
+	def postloop(self):
+		""""""
+	"""
+
+	def do_exit(self, args):
+		'''Exits the Taskmaster shell when user inputs "exit"'''
+		return -1
+
+	def default(self, line):
+		'''Custom input handling'''
+		if (line == "cheese"):
+			print "Crackers"
+		else if (line == "init"):
+			print "init";
+
+
+if __name__ == "__main__":
+	tm = Taskmaster()
+	os.system("clear")
+	tm.cmdloop()
